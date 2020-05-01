8da36603a5d25a4c077d7feb69b360a50e1a7452
Carlo Adap <carloa@campaigntrack.com>
2020-05-01 11:41:41 +0800

    [BI-1234]
    Added a default route and some comments

--- TestApiProject/Startup.cs
+++ TestApiProject/Startup.cs
@@ -25,6 +25,7 @@ namespace TestApiProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //ADDED SOME SAMPLE COMMENTS
             services.AddControllers();
         }
 
