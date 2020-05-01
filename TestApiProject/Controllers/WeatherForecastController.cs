cabb32a922f55ccb53a1333f475adc52961be750
Carlo Adap <carloa@campaigntrack.com>
2020-05-01 11:42:52 +0800

    Revert "[BI-1234]"
    This reverts commit 676f277f22328eab33f1093648ab7f0c564194b8.

--- TestApiProject/Controllers/WeatherForecastController.cs
+++ TestApiProject/Controllers/WeatherForecastController.cs
@@ -35,19 +35,5 @@ namespace TestApiProject.Controllers
             })
             .ToArray();
         }
-
-
-        [HttpGet("test-route")]
-        public IEnumerable<WeatherForecast> GetTestRoute()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
